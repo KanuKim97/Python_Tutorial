907dd3e722f35730dd903446d9c580beda1b1960
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2020-11-16 00:00:33 +0900

    Update 10_Silcing.py

--- 10_Silcing.py
+++ 10_Silcing.py
@@ -5,6 +5,6 @@ print("문장 : " + sentence[4:9])
 
 print("문장 : " + sentence[:10]) #처음부터 10 직전까지
 print("문장 : " + sentence[3:]) #3부터 끝까지
-print("문장 : " + sentence[-7:]) #뒤 부터 시작해서 7번쨰 앞까지
+print("문장 : " + sentence[-7:]) #뒤 부터 시작해서 7번째 앞까지
 
 print("글자 : " + sentence[0])
